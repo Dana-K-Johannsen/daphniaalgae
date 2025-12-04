d78558d49789e44d52707b30479cb4bea24b5354
Dana-K-Johannsen <gpk3qr@virginia.edu>
2025-12-04 12:08:26 -0500

    allele frequencies and fixing GDS

--- annotation/vcf_to_gds.R
+++ annotation/vcf_to_gds.R
@@ -1,11 +1,8 @@
-##installing and loading packages
-install.packages("BiocManager")
-BiocManager::install("SeqArray")
-library(SeqArray)
+library(SeqArray, lib.loc = "~/Rlibs")
 
 #define file paths
-vcf_file <- "/project/berglandlab/chlorella_sequencing/variant_calling/combined_chlorella_annotated.vcf"
-gds_file <- "/project/berglandlab/chlorella_sequencing/variant_calling/combined_chlorella_annotated.gds"
+vcf_file <- "/project/berglandlab/chlorella_sequencing/variant_calling/combined_chlorella_annotated.vcf" # nolint: line_length_linter.
+gds_file <- "/project/berglandlab/chlorella_sequencing/variant_calling/combined_chlorella_annotated.gds" # nolint: line_length_linter.
 
 #convert
 seqVCF2GDS(vcf.fn = vcf_file, out.fn = gds_file, parallel = TRUE)
